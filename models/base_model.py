e264cba928b0cf8e96e1b04629bd862eb45cd808
Mugisha Goal <degaullemugisha@gmail.com>
2023-10-15 14:05:38 -0700

    BaseModel linked with FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -16,14 +16,15 @@ class BaseModel:
         time_format = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
-                if key == 'created_at' or key == 'updated_at':
-                    setattr(self, key, datetime.strptime(value, time_format))
-                else:
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = self.updated_at = datetime.now()
-            storage.new(self)
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        setattr(self, key, datetime.strptime(value, time_format))
+                    else:
+                        setattr(self, key, value)
+                    else:
+                        self.id = str(uuid.uuid4())
+                        self.created_at = self.updated_at = datetime.now()
+                        storage.new(self)
 
     def save(self):
         """Updates the 'updated_at' attribute with the current datetime."""
