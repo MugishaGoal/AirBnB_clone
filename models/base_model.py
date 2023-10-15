c614228fa139a7e16e4db58be7bce7359637b502
Mugisha Goal <degaullemugisha@gmail.com>
2023-10-15 13:37:11 -0700

    BaseModel linked with FileStorage

--- models/base_model.py
+++ models/base_model.py
@@ -23,10 +23,12 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
 
     def save(self):
         """Updates the 'updated_at' attribute with the current datetime."""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
@@ -43,7 +45,6 @@ class BaseModel:
             "created_at": self.created_at.isoformat(),
             "updated_at": self.updated_at.isoformat()
         }
-
 
     def __str__(self):
         """Returns a string representation of the object."""
