84c4297713f0254ae6ca2c12237cee60fe02b6a0
Mugisha Goal <degaullemugisha@gmail.com>
2023-10-15 17:56:30 -0700

    A class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -69,3 +69,13 @@ class FileStorage:
                     class_name, obj_id = key.split(".")
                     obj = models.classes[class_name](**value)
                     self.__objects[key] = obj
+
+models.classes = {
+        'BaseModel': BaseModel,
+        'User': User,
+        'Place': Place,
+        'State': State,
+        'City': City,
+        'Amenity': Amenity,
+        'Review': Review,
+        }
